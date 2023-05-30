48f02f38f146b4c70c0b4a8936665e461c4ffc9b
oscmor8 <oscarmoradoo@gmail.com>
2023-05-30 09:56:35 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//Sync: means that the process runs only as a result of some other process being completed or handed off
+
+console.log("Me first");
+console.log("me second");
+console.log("me third");
+
+//Async: async operation means that a process operates independently of other processes
